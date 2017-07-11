3ebd5e862bf86340d3f40c4935dd6946b64f4929
difnajayashani <difna4@gmail.com>
2017-07-11 12:09:30 +0530

    Controller class updated

--- api_service/src/main/java/controller/StudentDataController.java
+++ api_service/src/main/java/controller/StudentDataController.java
@@ -2,9 +2,12 @@ package controller;
 
 import hsenid.com.domain.StudentData;
 import hsenid.com.repository.StudentDataRepositoryImpl;
+import models.SuccessMessage;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -27,14 +30,14 @@ public class StudentDataController {
     @RequestMapping(value = "/viewAll/", method = RequestMethod.GET, produces = {"application/json"})
     public ResponseEntity<List<StudentData>> listAllUsers() {
         List<StudentData> students = studentRepository.retrieveAllDetails();
-        
+
         SuccessMessage successMessage = new SuccessMessage();
         successMessage.setStatus("success");
         successMessage.setCode(HttpStatus.OK.value());
-            
-        if(students.isEmpty()){
-             successMessage.setMessage("No records found.");
-        }else{
+
+        if (students.isEmpty()) {
+            successMessage.setMessage("No records found.");
+        } else {
             successMessage.setMessage(students.size() + " records found.");
             for (int i = 0; i < students.size(); i++) {
                 JSONObject jsonObject = new JSONObject();
@@ -45,30 +48,48 @@ public class StudentDataController {
                 jsonObject.put("marksSubjectB", students.get(i).getMarksSubjectB());
 
                 successMessage.addData(jsonObject);
+            }
+            return new ResponseEntity<List<StudentData>>((List<StudentData>) successMessage, HttpStatus.OK);
+
         }
-        return new ResponseEntity<List<StudentData>>( successMessage, HttpStatus.OK);
+        return new ResponseEntity<List<StudentData>>(HttpStatus.NO_CONTENT);
     }
 
 
     //-------------------Retrieve Single User--------------------------------------------------------
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<List<StudentData>> getUser(@PathVariable("id") int id) {
-        System.out.println("Fetching Student with id " + id);
-        StudentData student = studentRepository.retrieveStudentDetail(id);
-        if (student == null) {
-            System.out.println("Student with id " + id + " not found");
-            return new ResponseEntity<List<StudentData>>(HttpStatus.NOT_FOUND);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {"application/json"})
+    public ResponseEntity<StudentData> getUser(@PathVariable("id") int id){
+
+            System.out.println("Fetching Student with id " + id);
+            StudentData student = studentRepository.retrieveStudentDetail(id);
+            SuccessMessage successMessage = new SuccessMessage();
+            successMessage.setStatus("success");
+            successMessage.setCode(HttpStatus.OK.value());
+
+            if (student == null) {
+                System.out.println("Student with id " + id + " not found");
+                successMessage.setMessage("no matching records found to delete.");
+                return new ResponseEntity<StudentData>(HttpStatus.NOT_FOUND);
+
+            }else {
+                successMessage.setMessage("student record is retrieved.");
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("student", student);
+                successMessage.addData(jsonObject);
+
+
+            }
+            return new ResponseEntity<StudentData>(student, HttpStatus.OK);
 
         }
-        //return new ResponseEntity<StudentData>(student, HttpStatus.OK);
-        return null;
-    }
+
+
 
     //-------------------Create a Student--------------------------------------------------------
 
-   @RequestMapping(value = "/insertOne/", method = RequestMethod.POST,, produces = {"application/json"}, consumes = {"application/json"})
-    public ResponseEntity<Void> insertStudent(@RequestBody StudentData student, UriComponentsBuilder ucBuilder) {
+   @RequestMapping(value = "/insert/", method = RequestMethod.POST, produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<Void> insertStudent( @RequestBody StudentData student) {
         System.out.println("Creating User " + student.getName());
         
          boolean status = studentRepository.addStudent(student);
@@ -79,27 +100,21 @@ public class StudentDataController {
             successMessage.setCode(HttpStatus.CREATED.value());
             successMessage.setMessage("new student added.");
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("student_id", students.getStudent_id());
-            jsonObject.put("name", students.getName());
-            jsonObject.put("age", students.getAge());
-            jsonObject.put("marksSubjectA", students.getMarksSubjectA());
-            jsonObject.put("marksSubjectB", students.getMarksSubjectB());
+            jsonObject.put("student_id", student.getStudent_id());
+            jsonObject.put("name", student.getName());
+            jsonObject.put("age", student.getAge());
+            jsonObject.put("marksSubjectA", student.getMarksSubjectA());
+            jsonObject.put("marksSubjectB", student.getMarksSubjectB());
             successMessage.addData(jsonObject);
           }
-            return new ResponseEntity<>(successMessage, HttpStatus.CREATED);
-
-        /*if (studentRepository.isUserExist(user)) {
-            System.out.println("A User with name " + user.getName() + " already exist");
-            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-        }
-
-        userService.saveUser(user);*/
+            return new ResponseEntity<Void>((MultiValueMap<String, String>) successMessage, HttpStatus.CREATED);
     }
         
         
     //-------------------Delete A Student--------------------------------------------------------
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = {"application/json"})
-    public ResponseEntity<Object> deleteStudent(@PathVariable("id") String studentId) {
+    public ResponseEntity<StudentData> deleteStudent(@PathVariable("id") int studentId) {
+
         int affectedRows = studentRepository.deleteStudent(studentId);
         SuccessMessage successMessage = new SuccessMessage();
         successMessage.setStatus("success");
@@ -111,13 +126,15 @@ public class StudentDataController {
             successMessage.addData(jsonObject);
         } else {
             successMessage.setMessage("no matching records found to delete.");
+            return new ResponseEntity<StudentData>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(successMessage, HttpStatus.OK);
+        return new ResponseEntity<StudentData>((MultiValueMap<String, String>) successMessage, HttpStatus.OK);
     }
         
   //------------------Update A Student--------------------------------------------------------
-    @RequestMapping(method = RequestMethod.PUT, produces = {"application/json"}, consumes = {"application/json"})
-    public ResponseEntity<Object> updateStudent(@RequestBody Student student) {
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT, produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<StudentData> updateStudent(@RequestBody StudentData student) {
+
         int affecteRows = studentRepository.updateStudent(student);
         SuccessMessage successMessage = new SuccessMessage();
         successMessage.setStatus("success");
@@ -125,12 +142,13 @@ public class StudentDataController {
         if(affecteRows>0){
             successMessage.setMessage("student record updated.");
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("userName", student.getUserName());
+            jsonObject.put("Student id", student.getStudent_id());
             successMessage.addData(jsonObject);
+            return new ResponseEntity<StudentData>(HttpStatus.NOT_FOUND);
         }else{
             successMessage.setMessage("no matching records found to update.");
         }
-        return new ResponseEntity<>(successMessage, HttpStatus.OK);
+        return new ResponseEntity<StudentData>((MultiValueMap<String, String>) successMessage, HttpStatus.OK);
 }
 
 
